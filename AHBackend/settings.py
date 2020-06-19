b6ca278bb2d04ab3b3cb642dde11a622ec99c22d
ajav06 <albert06av@hotmail.com>
2020-06-19 02:25:51 -0400

    Configs

--- AHBackend/settings.py
+++ AHBackend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wtn_4yh*v2tt+&u$aadhagl4wkhg6t(1v(tkx#8dpl2z5jqxc-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.107', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
