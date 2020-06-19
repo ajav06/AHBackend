c1df67e72d8152e186fc9c29637c7d3fdbe452c4
ajav06 <albert06av@hotmail.com>
2020-06-19 03:07:26 -0400

    update

--- AHBackend/settings.py
+++ AHBackend/settings.py
@@ -95,11 +95,19 @@ CORS_ALLOW_METHODS = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
